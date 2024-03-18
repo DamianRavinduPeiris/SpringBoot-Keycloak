803de1e24c3fbdc041951ea88d8e7db532037b06
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-03-18 12:56:16 +0530

    Initial Commit.

---
+++ src/main/java/com/damian/springkeycloak/SpringKeyclaokApplication.java
@@ -0,0 +1,13 @@
+package com.damian.springkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeyclaokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKeyclaokApplication.class, args);
+    }
+
+}
